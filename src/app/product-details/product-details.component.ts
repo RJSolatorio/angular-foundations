b61ec9f28d77d1eb4d3650bd47cc92af4337180b
Rj Solatorio <rjsolatorio@gmail.com>
2026-05-27 17:18:34 +0800

    Commit angular sample app.

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,25 @@
+import { Component, signal, input } from '@angular/core';
+import { IProduct } from '../product.model';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'bot-product-details',
+  imports: [CurrencyPipe],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css',
+})
+export class ProductDetailsComponent {
+  product = input.required<IProduct>();
+  availableInventory = signal(3);  
+
+  getImageUrl(product: IProduct): string {
+      return '/images/robot-parts/' + product.imageName;
+  }
+
+  addToCart(event: MouseEvent): void {
+    setTimeout(() => {
+      this.availableInventory.update(inventory => inventory - 1);
+    }, 100);
+  }
+  
+}
